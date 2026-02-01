626b77a76e1a82caa38d206bac997e70e3fd0f22
Kawaljeet Singh <141999694+kaws26@users.noreply.github.com>
2026-02-01 23:42:33 +0530

    Update vite.config.js

--- Frontend/schoolops/vite.config.js
+++ Frontend/schoolops/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://schoolops-cq1j.onrender.com/',
         changeOrigin: true
       }
     }
